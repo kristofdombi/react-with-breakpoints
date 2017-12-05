159088bb9c7d3a9b3c7d15e756f80d1bb694844d
kristof0425 <dombi.kristof@gmail.com>
2017-12-05 21:41:29 +0100

    refactor(HideAt): rewrite business logic

--- src/HideAt.js
+++ src/HideAt.js
@@ -1,47 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import withBreakpoint from './withBreakpoint';
+import setShouldRender from '../util/set-should-render';
 
 function HideAt({ breakpoint, currentBreakpoint, children }) {
   let shouldRender;
 
-  if (breakpoint.includes('small') && currentBreakpoint === 'small') {
-    shouldRender = false;
-  } else {
-    shouldRender = true;
-  }
-
-  if (breakpoint.includes('medium')) {
-    if (breakpoint.includes('AndBelow') && currentBreakpoint !== 'large') {
-      shouldRender = false;
-    } else if (breakpoint.includes('AndAbove') && currentBreakpoint !== 'small') {
-      shouldRender = false;
-    } else if (breakpoint === 'medium' && currentBreakpoint === 'medium') {
-      shouldRender = false;
-    }
-  }
-
-  if (breakpoint.includes('large')) {
-    if (breakpoint.includes('AndBelow')) {
-      shouldRender = false;
-    } else if (currentBreakpoint !== 'large') {
-      shouldRender = true;
-    } else {
-      shouldRender = false;
-    }
+  switch (currentBreakpoint) {
+    case 'small':
+      shouldRender = setShouldRender(['small', 'mediumAndBelow'], breakpoint);
+      break;
+    case 'medium':
+      shouldRender = setShouldRender(['medium', 'mediumAndAbove', 'mediumAndBelow'], breakpoint);
+      break;
+    case 'large':
+      shouldRender = setShouldRender(['mediumAndAbove', 'large'], breakpoint);
+      break;
   }
 
   if (shouldRender) {
     return (<div>{ children }</div>);
-    // TODO: solve unnecessary else after return
-    // eslint-disable-next-line
-  } else {
-    return null;
   }
+  return null;
 }
 
 HideAt.propTypes = {
-  breakpoint: PropTypes.oneOf(['small', 'medium', 'mediumAndBelow', 'mediumAndAbove', 'largeAndBelow', 'large']).isRequired,
+  breakpoint: PropTypes.oneOf(['small', 'medium', 'mediumAndBelow', 'mediumAndAbove', 'large']).isRequired,
   breakpoints: PropTypes.object,
   currentBreakpoint: PropTypes.string,
   children: PropTypes.node,
