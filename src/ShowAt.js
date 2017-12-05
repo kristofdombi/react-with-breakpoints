2450c1ba576a7fcbf15b2ee35da1f240c66a9299
kristof0425 <dombi.kristof@gmail.com>
2017-12-05 21:42:24 +0100

    feat(ShowAt): add refactored ShowAt

---
+++ src/ShowAt.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import withBreakpoint from './withBreakpoint';
+import setShouldRender from '../util/set-should-render';
+
+function ShowAt({ breakpoint, currentBreakpoint, children }) {
+  let shouldRender;
+
+  switch (currentBreakpoint) {
+    case 'small':
+      shouldRender = setShouldRender(['small', 'mediumAndBelow'], breakpoint, false);
+      break;
+    case 'medium':
+      shouldRender = setShouldRender(['medium', 'mediumAndAbove', 'mediumAndBelow'], breakpoint, false);
+      break;
+    case 'large':
+      shouldRender = setShouldRender(['mediumAndAbove', 'large'], breakpoint, false);
+      break;
+  }
+
+  if (shouldRender) {
+    return (<div>{ children }</div>);
+  }
+  return null;
+}
+
+ShowAt.propTypes = {
+  breakpoint: PropTypes.oneOf(['small', 'medium', 'mediumAndBelow', 'mediumAndAbove', 'large']).isRequired,
+  breakpoints: PropTypes.object,
+  currentBreakpoint: PropTypes.string,
+  children: PropTypes.node,
+};
+
+ShowAt.displayName = 'ShowAt';
+
+ShowAt.defaultProps = {
+  breakpoint: '',
+  currentBreakpoint: '',
+  children: null,
+};
+
+const ShowAtWithBreakpoint = withBreakpoint(ShowAt);
+
+export default ShowAtWithBreakpoint;
