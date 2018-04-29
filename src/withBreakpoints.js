6464f60e5ef43bbbabb424d1dd755f1b11ee5408
kristof0425 <dombi.kristof@gmail.com>
2018-04-29 12:30:20 +0200

    refactor(withBreakpoints): leverage the new context api

--- src/withBreakpoints.js
+++ src/withBreakpoints.js
@@ -2,63 +2,83 @@ import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import airbnbBreakpoints from '../util/airbnb-breakpoints';
 
-export default function withBreakpoints(WrappedComponent) {
-  class WithBreakpoints extends PureComponent {
-    state = {
-      currentBreakpoint: '',
-    }
+const Context = React.createContext();
 
-    componentDidMount() {
-      window.addEventListener('resize', this.handleResize, { passive: true });
-      this.handleResize();
-    }
+export const withBreakpointsContext = (WrappedComponent) => {
+  const Component = (props) => (
+    <Context.Consumer>
+      { payload => (
+        <WrappedComponent
+          { ...props }
+          currentBreakpoint={ payload.currentBreakpoint }
+        />
+      ) }
+    </Context.Consumer>
+  );
+  Component.displayName = `withBreakpoints(${
+    WrappedComponent.displayName
+      || WrappedComponent.name
+      || 'Component'
+  })`;
+  return Component;
+};
 
-    componentWillUnmount() {
-      window.removeEventListener('resize', this.handleResize);
-    }
+export default class BreakpointsProvider extends PureComponent {
+  static displayName = 'BreakpointsProvider';
 
-    handleResize = () => {
-      const clientWidth = window.innerWidth;
-      const { breakpoints } = this.props;
-      if (clientWidth < breakpoints.small) {
-        this.setState({
-          currentBreakpoint: 'small',
-        });
-      } else if (clientWidth < breakpoints.medium) {
-        this.setState({
-          currentBreakpoint: 'medium',
-        });
-      } else if (clientWidth <= breakpoints.large || clientWidth > breakpoints.large) {
-        this.setState({
-          currentBreakpoint: 'large',
-        });
-      }
-    }
-
-    render() {
-      return (
-        <WrappedComponent currentBreakpoint={ this.state.currentBreakpoint } { ...this.props } />
-      );
-    }
-  }
-  const wrappedComponentName = WrappedComponent.displayName
-    || WrappedComponent.name
-    || 'Component';
-
-  WithBreakpoints.WrappedComponent = WrappedComponent;
-  WithBreakpoints.displayName = `withBreakpoint(${wrappedComponentName})`;
-
-  WithBreakpoints.propTypes = {
+  static propTypes = {
     breakpoints: PropTypes.shape({
       small: PropTypes.number.isRequired,
       medium: PropTypes.number.isRequired,
       large: PropTypes.number.isRequired,
     }),
+    children: PropTypes.node,
   };
 
-  WithBreakpoints.defaultProps = {
+  static defaultProps = {
     breakpoints: airbnbBreakpoints,
   };
 
-  return WithBreakpoints;
+  state = {
+    currentBreakpoint: '',
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.handleResize, { passive: true });
+    this.handleResize();
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleResize);
+  }
+
+  handleResize = () => {
+    const clientWidth = window.innerWidth;
+    const { breakpoints } = this.props;
+    if (clientWidth < breakpoints.small) {
+      this.setState({
+        currentBreakpoint: 'small',
+      });
+    } else if (clientWidth < breakpoints.medium) {
+      this.setState({
+        currentBreakpoint: 'medium',
+      });
+    } else if (clientWidth <= breakpoints.large || clientWidth > breakpoints.large) {
+      this.setState({
+        currentBreakpoint: 'large',
+      });
+    }
+  }
+
+  render() {
+    return (
+      <Context.Provider
+        value={ {
+          currentBreakpoint: this.state.currentBreakpoint,
+        } }
+      >
+        { this.props.children }
+      </Context.Provider>
+    );
+  }
 }
