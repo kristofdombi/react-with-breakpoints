13615cf69ea8203b5a33af7c9c5a569022f6c07d
kristof0425 <dombi.kristof@gmail.com>
2018-04-30 13:39:30 +0200

    feat(Landing, Story): add signature to site

--- docs/sections/landing.js
+++ docs/sections/landing.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Logo from '../components/logo';
 import Button from '../components/button';
 import Card from '../components/card';
+import Signature from '../components/signature';
 import Code, { OrangeSyntax, CommentSyntax } from '../components/code';
 import PropTypes from 'prop-types';
 import { ShowAt } from '../../src/react-with-breakpoints';
@@ -19,6 +20,9 @@ const Landing = ({ onChange }) => (
         ~ yarn add <OrangeSyntax>react-with-breakpoints</OrangeSyntax>
       </Code>
     </Card>
+    <ShowAt breakpoint="small">
+      <Signature />
+    </ShowAt>
     <ShowAt breakpoint="small">
       <Button onClick={ onChange }>Documentation 👉</Button>
     </ShowAt>
